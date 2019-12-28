62e24c6391c98a30ee5401ed4a349c16c9d813cb
Sam Levy <slevy239@gmail.com>
2019-12-28 16:13:43 -0500

    Updates

---
+++ precache-manifest.ebbc23517a3e2b94a1db0751a919e932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c70b9c0bd9e9477986c972e48ecf26ac",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "a5a108654bb4216121d0",
+    "url": "/Clicky-Game/static/css/main.db8b15a2.chunk.css"
+  },
+  {
+    "revision": "da2148e6f4bb4e003925",
+    "url": "/Clicky-Game/static/js/2.4113a606.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.4113a606.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5a108654bb4216121d0",
+    "url": "/Clicky-Game/static/js/main.f95a62d4.chunk.js"
+  },
+  {
+    "revision": "dca2329584f9229ae790",
+    "url": "/Clicky-Game/static/js/runtime-main.3758ee11.js"
+  }
+]);
