ac3e96812ea96283440b2d010d8622e08683036b
Sam Levy <slevy239@gmail.com>
2019-12-30 12:46:49 -0500

    format

--- src/App.js
+++ src/App.js
@@ -11,65 +11,56 @@ class App extends Component {
 	state = {
 		friends,
 		clickedId: [],
-		TopScore: [ 0 ],
+		TopScore: [0],
 		maxScore: 0,
 		score: 0,
 		goal: 10,
-		status: 'Click an image to begin the game!'
-	};
+		status: ''
+	}
+
 
 	componentDidMount() {
 		this.shuffleCard(this.state.friends);
 	}
 
 	shuffleCard = (id) => {
+
 		let clickedId = this.state.clickedId;
 
 		if (clickedId.includes(id)) {
-			let maxScore = Math.max(...this.state.TopScore);
-			let filteredTopScores = this.state.TopScore.filter((num) => num >= Math.max(...this.state.TopScore));
-			this.setState({
-				clickedId: [],
-				score: 0,
-				TopScore: filteredTopScores,
-				maxScore: maxScore,
-				status: 'WRONG!'
-			});
+			this.setState({ clickedId: [], score: 0, status: "GG", })
 			return;
 		} else {
 			clickedId.push(id);
-			this.state.TopScore.push(this.state.clickedId.length);
-			this.setState({
-				status: 'Correct!',
-				score: this.state.score + 1
-			});
+
 			if (clickedId.length === 10) {
-				this.setState({ score: 10, clickedId: [], status: 'You won!' });
+				this.setState({ score: 10, status: "Winner", clickedId: [] })
 				return;
 			}
-			this.setState({ clickedId, score: this.state.score + 1 });
+			this.setState({ friends, clickedId, score: clickedId.length, status: " " });
 
-			for (let i = 0; i < friends.length; i++) {
+
+			for (let i = friends.length - 1; i > 0; i++) {
 				let j = Math.floor(Math.random() * (i + 1));
-				[ friends[i], friends[j] ] = [ friends[j], friends[i] ];
+				[friends[i], friends[j]] = [friends[j], friends[i]];
+
 			}
 		}
-	};
-	// Map over this.state.friends and render a Card component for each friend object
+
+	}
 	render() {
 		return (
 			<div>
-				<Navbar total={this.state.score -1} goal={this.state.goal} maxScore={this.state.maxScore} />
+				<Navbar total={this.state.score} goal={this.state.goal} maxScore={this.state.maxScore} />
 				<Header status={this.state.status} />
 				<Container>
-					{this.state.friends.map((friend) => (
-						<Card shuffleCard={this.shuffleCard} image={friend.image} />
-					))}
+					{this.state.friends.map((friend) => <Card shuffleCard={this.shuffleCard} image={friend.image} />)}
 				</Container>
 				<Footer />
 			</div>
 		);
 	}
 }
+
 
 export default App;
