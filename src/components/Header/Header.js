09ff0f6c7d1bd707a75f37ea6f262ea9300aa113
Sam Levy <slevy239@gmail.com>
2019-12-30 23:56:10 -0500

    css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './Header.css'
 const Header = (props) => {
 	return (
 		<div className="jumbotron jumbotron-fluid">
 			<div className="container text-center">
-				<h1 className="display-4 header-main">Clicky Game</h1>
-				<h2 className="display-6">Try not to click the same image twice!</h2>
+				<h1 className="display-4 header-main">Click a 76er Game</h1>
+				<h2 className="display-6">Try not to click the same player twice!</h2>
 				<h5 className="display-6">{props.status}</h5>
 			</div>
 		</div>
